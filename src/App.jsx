7a5c47c2831ff49f026ebaa515647b06dc9d913f
Jinalee <jinaleesubasinghe16@gmail.com>
2026-03-04 14:19:54 +0530

    fix: switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Nav from './components/Nav'
 import Home from './pages/Home'
 import Stats from './pages/Stats'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="app-bg">
         <div style={{ position: 'relative', zIndex: 1, maxWidth: '680px', margin: '0 auto', padding: '28px 20px 80px' }}>
           <Nav />
@@ -15,6 +15,6 @@ export default function App() {
           </Routes>
         </div>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
